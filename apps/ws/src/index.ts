695d4b999469a42405242f9c18bd1a6c0dfc7363
suleman1412 <sulemankarigar1412@gmail.com>
2025-03-23 15:44:05 +0000

    ws

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,15 +1,73 @@
-import { WebSocketServer } from 'ws'
+import { WebSocket, WebSocketServer } from 'ws'
 
 const wss = new WebSocketServer({ port: 8080 })
 
+interface messageType{
+  type: 'join_room' | 'chat' | 'leave_room',
+  roomId?: string,
+  message?: string 
+}
+
+const rooms: { [roomId: string]: WebSocket[] } = {}
+
+const join_room = (ws: WebSocket, roomId: string) => {
+  if(!rooms[roomId]){
+    rooms[roomId] = []
+  }
+
+  rooms[roomId].push(ws)
+  console.log(`User joined room: ${roomId}. Total users: ${rooms[roomId].length}`);
+}
+
+const leave_room = (ws: WebSocket, roomId: string) => {
+  if (!rooms[roomId]) {
+    return;
+  }
+
+  rooms[roomId] = rooms[roomId].filter(client => client !== ws);
+  console.log(`User left room: ${roomId}. Remaining users: ${rooms[roomId].length}`);
+}
+
+const chat_room = (ws: WebSocket, roomId: string, message: string) => {
+  if (!rooms[roomId]) {
+    return;
+  }
+
+  rooms[roomId].forEach(client => {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+};
+
+
+
 wss.on('connection', (socket) => {
- 
-  socket.on('open', () => {
-    console.log('Connection opened')
-  })
+  console.log('New server joined')
 
   socket.on('message', (message) => {
-    console.log(`Received message => ${message}`)
-    socket.send(`You sent => ${message}`)
+    try {
+      const parsedMessage: messageType = JSON.parse(message.toString())
+      if (parsedMessage.roomId && parsedMessage.type) {
+        if(parsedMessage.type === 'join_room'){
+          join_room(socket, parsedMessage.roomId)
+        } else if (parsedMessage.type === 'leave_room'){
+          leave_room(socket, parsedMessage.roomId)
+        } else if(parsedMessage.type === 'chat'){
+          if(!parsedMessage.message){
+            socket.send(JSON.stringify({ error: "Message is required" }))
+            return
+          }
+          chat_room(socket, parsedMessage.roomId, parsedMessage.message)
+        }
+      } else{
+        socket.send(JSON.stringify({ error: 'RoomId or Type not provided' }))
+        return
+      }
+    } catch (e) {
+      console.error(e)
+      socket.send(JSON.stringify({ error: e }))
+    }
   })
+
 })
